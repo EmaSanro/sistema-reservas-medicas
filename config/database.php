4a5576e4436aa27f96ad215fac17c910784ff0d6
Emanuel San Roman <emanuel.sanroman03@gmail.com>
2026-02-12 12:13:53 -0300

    agregando rutas al gitignore | cambiando a patron singleton en database

--- config/database.php
+++ config/database.php
@@ -3,23 +3,27 @@ namespace AppConfig;
 
 use Dotenv\Dotenv;
 use PDO;
+use PDOException;
 
 class Database {
-    // private static $db;
+    private static $db = null;
     
     public static function getConnection() {
-
-        $dotenv = Dotenv::createImmutable(__DIR__ . '/../'); 
-        $dotenv->load();
-
-        $DB_HOST = $_ENV["DB_HOST"];
-        $DB_PORT = $_ENV["DB_PORT"];
-        $DB_NAME = $_ENV["DB_NAME"];
-        $DB_USER = $_ENV["DB_USER"];
-        $DB_PASS = $_ENV["DB_PASS"];
-        if(!isset($db)) {
-            $db = new PDO("mysql:host=$DB_HOST;port=$DB_PORT;dbname=$DB_NAME;charset=utf8mb4", $DB_USER, $DB_PASS);
+        if(self::$db == null) {
+            $dotenv = Dotenv::createImmutable(__DIR__ . '/../'); 
+            $dotenv->load();
+    
+            $DB_HOST = $_ENV["DB_HOST"];
+            $DB_PORT = $_ENV["DB_PORT"];
+            $DB_NAME = $_ENV["DB_NAME"];
+            $DB_USER = $_ENV["DB_USER"];
+            $DB_PASS = $_ENV["DB_PASS"];
+            try {
+                self::$db = new PDO("mysql:host=$DB_HOST;port=$DB_PORT;dbname=$DB_NAME;charset=utf8mb4", $DB_USER, $DB_PASS);
+            } catch (\Throwable $th) {
+                throw new PDOException("Error al establecer conexion" . $th->getMessage());
+            }
         }
-        return $db;
+        return self::$db;
     }
 }
